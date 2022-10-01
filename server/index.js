24a16d048e5f0495b3de22382d2284e4de4d8730
Sunil Kumar <sunilk10099@gmail.com>
2022-10-01 23:58:46 +0530

    add basic express server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//base api endpoint
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    message: "all resources are online",
+  });
+});
+
+//no resource found
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "no resource found with this endpoint",
+  });
+});
+
+//server listening at port
+app.listen(port, (err) => {
+  if (err) throw err;
+
+  console.log(`server listening at port ${port}`);
+});

21c16f25cf88b32cac6280bb4dde9390fc430f94
Sunil Kumar <sunilk10099@gmail.com>
2022-10-02 19:15:44 +0530

    create basic user controllers

---
+++ server/Controllers/User.js
@@ -0,0 +1,26 @@
+//get user from the database
+const loginUser = (req, res) => {
+  res.send("ok");
+};
+
+//create user in database
+const signUpUser = (req, res) => {
+  res.send("ok");
+};
+
+//update user in database
+const updateUser = (req, res) => {
+  res.send("ok");
+};
+
+//delete user in database
+const deleteUser = (req, res) => {
+  res.send("ok");
+};
+
+module.exports = {
+  loginUser,
+  signUpUser,
+  updateUser,
+  deleteUser,
+};

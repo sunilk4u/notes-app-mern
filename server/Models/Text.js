13cf15f5309efd17ddd1ed0c6690e68efc3d9a9c
Sunil Kumar <sunilk10099@gmail.com>
2022-10-03 20:02:11 +0530

    add text model

---
+++ server/Models/Text.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//text file schema
+const textSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//create text model
+const Text = mognoose.model("text", textSchema);
+
+module.exports = Text;

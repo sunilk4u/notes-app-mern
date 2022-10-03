d23d4b8c045bc77cbe77c8eceeab866df45b1df7
Sunil Kumar <sunilk10099@gmail.com>
2022-10-03 21:57:56 +0530

    add read text file logic

--- server/Controllers/Text.js
+++ server/Controllers/Text.js
@@ -1,8 +1,35 @@
+const fs = require("fs");
 const ErrorHandler = require("../Utils/ErrorHandler");
+const Text = require("../Models/Text");
+const fileDir = "./Data/";
 
 //get text data into file
-const fetchData = (req, res) => {
-  res.send("ok");
+const fetchData = async (req, res) => {
+  const { _id } = req.body;
+
+  //check if text is provided or not
+  if (!_id) {
+    return ErrorHandler(req, res, 400, "Text id not provided");
+  } else {
+    const text = Text.findOne({ _id: _id });
+
+    //check if text file is present with given id
+    if (!text) {
+      return ErrorHandler(req, res, 404, "File not found in directory");
+    } else {
+      //read data from file and send it to client
+      console.log(fileDir + _id + ".txt");
+      fs.readFile(fileDir + _id + ".txt", "utf-8", function (err, data) {
+        if (err) {
+          console.log(err);
+          return ErrorHandler(req, res, 500, "Unable to read data");
+        }
+
+        res.set("Content-Type", "text/plain");
+        res.status(200).send(data);
+      });
+    }
+  }
 };
 
 //write text data into file

1de1a6b5e49472b676faadf0376cf3d83236d086
Sunil Kumar <sunilk10099@gmail.com>
2022-10-03 14:58:12 +0530

    create update user data controller

--- server/Controllers/User.js
+++ server/Controllers/User.js
@@ -82,8 +82,55 @@ const signUpUser = async (req, res) => {
 };
 
 //update user in database
-const updateUser = (req, res) => {
-  res.send("ok");
+const updateUser = async (req, res) => {
+  const { _id, name, email, password } = req.body;
+
+  //if id is not provided then reject the request
+  if (!_id) {
+    return ErrorHandler(
+      req,
+      res,
+      400,
+      "Request is rejected because of invalid user id."
+    );
+  } else {
+    const user = await User.findById(_id);
+
+    //check if user is present with given id
+    if (!user) {
+      return ErrorHandler(req, res, 404, "User not found.");
+    } else {
+      //if name is provided and validates then update name
+      if (name && name.length > 3) {
+        user.name = name;
+      }
+      //if email is provided and validates then update name
+      if (
+        email &&
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+      ) {
+        user.email = email;
+      }
+      //if password is provided and validates then update name
+      if (password && (password.length < 8 || !/\d/.test(password))) {
+        user.password = password;
+      }
+
+      //after updating values save the data in database
+      user.save((err) => {
+        if (err) {
+          return ErrorHandler(req, res, 500, err.message);
+        }
+
+        res.status(200).json({
+          _id: _id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+        });
+      });
+    }
+  }
 };
 
 //delete user in database

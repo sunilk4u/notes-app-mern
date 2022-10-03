7fdeaaa50f590f4eb8756463200aaa70d98aa778
Sunil Kumar <sunilk10099@gmail.com>
2022-10-03 20:02:34 +0530

    add controllers to text route

--- server/Routes/Text.js
+++ server/Routes/Text.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const { fetchData, writeData } = require("../Controllers/Text");
 
 //fetch data from user's file
-router.post("/fetch");
+router.post("/fetch", fetchData);
 
 //write the updated data into file
-router.put("/write");
+router.put("/write", writeData);
 
 module.exports = router;

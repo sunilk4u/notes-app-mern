0e6214d51acc252e0bc568b1c0e9cd10f64511de
Sunil Kumar <sunilk10099@gmail.com>
2022-10-04 18:04:51 +0530

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header"
 import LoginPage from "./pages/Login/LoginPage";
 
 const App = () => {
   return (
     <Router>
       <div className="App">
+        <Header />
+
+        {/* page routes */}
         <Routes>
           <Route exact path="/" element={<LoginPage />} />
         </Routes>
+
+        <Footer />
       </div>
     </Router>
   );
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <footer className="copyright">
+        <a href="https://github.com/sunilk4u" className="copyright_link">&copy; Sunil K.B.</a>
+    </footer>
+  )
+}
+
+export default Footer

a69404557b983aa5987173731e88f92da1b7da45
Sunil Kumar <sunilk10099@gmail.com>
2022-10-04 15:30:26 +0530

    add delete file route

--- server/Routes/Text.js
+++ server/Routes/Text.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { fetchData, writeData } = require("../Controllers/Text");
+const { fetchData, writeData, deleteFile } = require("../Controllers/Text");
 
 //fetch data from user's file
 router.post("/fetch", fetchData);
 
 //write the updated data into file
 router.put("/write", writeData);
+
+//delete file
+router.delete("/delete", deleteFile)
 
 module.exports = router;

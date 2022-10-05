63f71aa29d5b48f5f92b2abc2c1ff458a2be8ed4
Sunil Kumar <sunilk10099@gmail.com>
2022-10-05 18:00:22 +0530

    add login button link

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import {Link} from "react-router-dom"
 import "./style.css";
 
 const Header = () => {
@@ -8,7 +9,7 @@ const Header = () => {
       <div className="header container">
         <div className="header_logo">My Notes</div>
         <div className="auth_buttons">
-          <Button color="primary" variant="contained">Log in</Button>
+          <Button color="primary" variant="contained" component={Link} to="/">Log in</Button>
           <Button variant="outlined">Sign Up</Button>
         </div>
       </div>

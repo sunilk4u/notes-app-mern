e415c44fe9af2211954a224ed06b3d663c98731c
Sunil Kumar <sunilk10099@gmail.com>
2022-10-07 12:17:36 +0530

    add signup and redirect logic

--- client/src/components/AuthForm/SignUpForm.jsx
+++ client/src/components/AuthForm/SignUpForm.jsx
@@ -1,33 +1,79 @@
-import { Button, TextField } from "@mui/material";
-import React from "react";
+import { Alert, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { userSignup } from "../../redux/userSlice";
 
 const SignUpForm = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { status, error_message, isLoggedIn } = useSelector(
+    (state) => state.user
+  );
+  const dispatch = useDispatch();
+
+  //is logged in as user then redirect to notes page
+  if (isLoggedIn) {
+    return <Navigate to="/notes" />;
+  }
+
+  //perform signup operation
+  const handleSignUp = () => {
+    if (status === "pending") {
+      return;
+    } else {
+      dispatch(userSignup(formValues));
+    }
+  };
+
   return (
     <div className="login_form">
+      {status === "error" && (
+        <Alert severity="error">
+          {error_message || "cannot connect to server"}
+        </Alert>
+      )}
       <h6 className="login_label">Sign up</h6>
       <TextField
         required
-        id="filled-basic"
+        id="name"
         label="Name"
         variant="filled"
+        value={formValues.name}
+        onChange={(e) =>
+          setFormValues({ ...formValues, name: e.currentTarget.value })
+        }
         helperText="Name must have more than 3 chars"
       />
       <TextField
         required
-        id="filled-basic"
+        id="email"
         label="Email"
         variant="filled"
+        value={formValues.email}
+        onChange={(e) =>
+          setFormValues({ ...formValues, email: e.currentTarget.value })
+        }
         helperText="Enter a valid email"
       />
       <TextField
         required
-        id="filled-basic"
+        id="password"
         label="Password"
         variant="filled"
         type="password"
+        value={formValues.password}
+        onChange={(e) =>
+          setFormValues({ ...formValues, password: e.currentTarget.value })
+        }
         helperText="Password must contain 8 chars and a number"
       />
-      <Button variant="contained">Sign up</Button>
+      <Button variant="contained" onClick={handleSignUp}>
+        Sign up
+      </Button>
     </div>
   );
 };

01b7fb1a422d0be5df3ef03831ab57a60b3612cd
Sunil Kumar <sunilk10099@gmail.com>
2022-10-07 11:29:48 +0530

    add user account button in header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./style.css";
+
+//header menu items
+const HeaderMenu = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  //if logged in then show user account button
+  if (isLoggedIn) {
+    return (
+      <Button variant="outlined" component={Link} to="/user">
+        Account
+      </Button>
+    );
+  } else {
+    return (
+      <>
+        <Button variant="contained" component={Link} to="/">
+          Log in
+        </Button>
+        <Button variant="outlined" component={Link} to="/signup">
+          Sign Up
+        </Button>
+      </>
+    );
+  }
+};
 
 const Header = () => {
   return (
@@ -9,10 +35,7 @@ const Header = () => {
       <div className="header container">
         <div className="header_logo">My Notes</div>
         <div className="auth_buttons">
-          <Button variant="contained" component={Link} to="/">
-            Log in
-          </Button>
-          <Button variant="outlined" component={Link} to="/signup">Sign Up</Button>
+          <HeaderMenu />
         </div>
       </div>
     </div>
